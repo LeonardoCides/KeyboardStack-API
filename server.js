05025e1a2795b2716dc42d4b7e91ce2d47d0e76e
LeonardoCides <leonardo.cides.pro@gmail.com>
2026-02-24 20:55:02 -0300

    Refactor estoque routes and error handling

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
+
+//IMPORTAÇÕES
 const estoque = require('./estoque');
 const realizarVenda = require("./vendas")
 
+//MIDDLEWARE
 app.use(express.json());
 
+//ROTA INICIAL
 app.get("/" ,(req,res) => {
     res.send("Server on");
 });
+
+//LISTAR TODO O ESTOQUE
+app.get("/estoque", (req,res) => {
+    res.status(200).json(estoque);
+    
+});
+//CRIAR NOVO PRODUTO
 app.post("/estoque", (req, res) => {
     const novoProduto = req.body;
     if(!novoProduto){
@@ -15,6 +26,8 @@ app.post("/estoque", (req, res) => {
     }
     res.send(estoque.push(novoProduto))
 });
+
+//VENDER ITEM (ATUALIZACAO PARCIAL)
 app.patch("/estoque/vender/:id", (req, res) => {
     try{
         const produtoAtualizado = realizarVenda(Number(req.params.id));
@@ -24,20 +37,20 @@ app.patch("/estoque/vender/:id", (req, res) => {
         res.status(status).json({erro: e.message})
     }
 });
+
+//DELETAR UM PRODUTO DO ESTOQUE
 app.delete("/estoque/:id", (req, res) =>{
     const id = Number(req.params.id);
     const index = estoque.findIndex(p => p.id === id);
     if(index === -1){
-        return res.status(404).json({mensagem: "ID informado inexistente!"});
-    }
+        res.status(404).json({Mensagem: "Id inexistente."})
+    };
     estoque.splice(index, 1);
     res.status(200).json("Item excluido com sucesso!");
-});
-app.get("/estoque", (req,res) => {
-    res.status(200).json(estoque);
-    
-});
+})
+
+// START SERVER
 app.listen(3000, ()=>{
     console.log("Server rodando na porta 3000.")
-})
+});
 
