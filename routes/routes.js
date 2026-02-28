55b171a8c9ce940e0555c9b4136e11c1f413c573
LeonardoCides <leonardo.cides.pro@gmail.com>
2026-02-27 22:54:49 -0300

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,5 @@
 const express = require("express");
 const { db, setupDatabase} = require("../mysql-init");
 const router = express.Router();
-// ROTA INICIAL
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 module.exports = router;
 
