6b4bad7bb4c6e51a70ce3851955899c7d5616587
LeonardoCides <leonardo.cides.pro@gmail.com>
2026-02-28 21:50:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,25 +4,27 @@ const { db, setupDatabase} = require("./mysql-init");
 
 const app = express();
 app.use(express.json());
-app.use(express.static("public"));
-require("dotenv").config();
+app.use(express.static('public'));
+app.use(require('cors')());
 
-//CONFIG DO ENV
+//Importando config do ENV
+require("dotenv").config();
 const PORT = process.env.PORT;
 
 // Importando ROTAS
 const rotasGerais = require("./routes/routes");
 const rotasEstoque = require("./routes/estoque");
+const rotasVendas = require("./routes/vendas");
+const analiseRoutes = require("./routes/analise");
+
 //Importando Banner
 const banner = require("./chalk");
 
-
-const analiseRoutes = require("./routes/analise");
 app.use("/analise", analiseRoutes);
 
 app.use("/", rotasGerais);
-app.use("/estoque", rotasEstoque)
-
+app.use("/estoque", rotasEstoque);
+app.use("/vendas", rotasVendas);
 console.clear()
 
 app.listen(PORT || 4000, () => {
@@ -34,4 +36,3 @@ setupDatabase();
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
